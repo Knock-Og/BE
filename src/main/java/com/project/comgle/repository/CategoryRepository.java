ce01603e41d4d9d195d88639e6b08e9fa5a2943d
Parkjaeyong <pjjy92@gmail.com>
2023-03-15 14:28:52 +0900

    Feat : 카테고리 추가 기능 (#27) (#32)

--- src/main/java/com/project/comgle/repository/CategoryRepository.java
+++ src/main/java/com/project/comgle/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import com.project.comgle.entity.Category;
 import com.project.comgle.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository <Category,Long> {
 
+    Optional<Category> findByCategoryName(String categoryName);
 }
