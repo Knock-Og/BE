50053972f80440cf94ac20225af5e2d0e52592aa
Parkjaeyong <pjjy92@gmail.com>
2023-03-15 15:53:54 +0900

    Fix : 회원 승인 및 미승인 회원 전체 조회 기능 삭제 #35 (#36)
    - 미승인 회원 전체 조회하는 기능 제거
    - 회원 승인기능 제거

--- src/main/java/com/project/comgle/controller/AdminController.java
+++ src/main/java/com/project/comgle/controller/AdminController.java
@@ -2,28 +2,18 @@ package com.project.comgle.controller;
 
 import com.project.comgle.dto.request.PositionRequestDto;
 import com.project.comgle.dto.response.MessageResponseDto;
-import com.project.comgle.dto.response.PermissionResponseDto;
 import com.project.comgle.service.AdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
+
     private final AdminService adminService;
-
-    // 미승인 회원 전체 조회
-    @GetMapping("/hold/members")
-    public List<PermissionResponseDto> getPermissionMembers(){
-        return adminService.findPermisionMembers();
-    }
-
-    @PostMapping("/permision/members/{member-id}")
-    public MessageResponseDto permision(@PathVariable(name = "member-id") Long memberId){
-        return adminService.permisionMember(memberId);
-    }
 
     @PutMapping("/position/members/{member-id}")
     public MessageResponseDto updatePosition(@PathVariable(name = "member-id") Long memberId, @RequestBody PositionRequestDto positionRequestDto){
--- src/main/java/com/project/comgle/service/AdminService.java
+++ src/main/java/com/project/comgle/service/AdminService.java
@@ -19,37 +19,6 @@ public class AdminService {
 
     private final MemberRepository memberRepository;
 
-    // 미승인 회원 전체 조회 기능
-    @Transactional(readOnly = true)
-    public List<PermissionResponseDto> findPermisionMembers() {
-        // 개발 단계 -> 추후 permision false인 멤버들 반환
-        List<Member> findMembers = memberRepository.findAllByPermission(false);
-        List<PermissionResponseDto> permissionResponseDtos = new ArrayList<>();
-
-        for (Member member : findMembers) {
-            permissionResponseDtos.add(PermissionResponseDto.from(member));
-        }
-
-        return permissionResponseDtos;
-    }
-
-    // 회원가입 요청 회원 승인 기능
-    @Transactional
-    public MessageResponseDto permisionMember(Long memberId) {
-
-        Member findMember = memberRepository.findById(memberId).orElseThrow(
-                () -> new IllegalStateException("해당 멤버가 없습니다.")
-        );
-
-        if(findMember.isPermission()){
-            throw new IllegalStateException("이미 승인된 유저입니다.");
-        }
-
-        findMember.permision();
-
-        return MessageResponseDto.of(HttpStatus.OK.value(), "승인 완료");
-    }
-
     // 회원 직책 수정 기능
     @Transactional
     public MessageResponseDto updatePosition(Long memberId, String pos) {
