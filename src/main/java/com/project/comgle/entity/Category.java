100186e4af0e6547a773d6d3077d6d84ebe345f4
Sunyeon-Jeong <sunyeon4012@gmail.com>
2023-03-18 00:35:49 +0900

    Refactor : 댓글삭제 구현코드 리팩토링 (#74) - Category.java Entity push

---
+++ src/main/java/com/project/comgle/entity/Category.java
@@ -0,0 +1,43 @@
+package com.project.comgle.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String categoryName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPANY_ID", nullable = false)
+    private Company company;
+
+    //Post 담당자와 추후 협의할 사항
+    @Builder
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Builder
+    public Category(String categoryName, Company company) {
+        this.categoryName = categoryName;
+        this.company = company;
+    }
+
+    public void update(String categoryName){
+        this.categoryName = categoryName;
+    }
+
+
+}
