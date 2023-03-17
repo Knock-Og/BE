57440d5def61c4c44a6ad93b4db5e0e45649412d
hunny-A <91798823+hunny-A@users.noreply.github.com>
2023-03-17 19:02:54 +0900

    Refactor : 즐겨찾기 전체 기능에 바뀐 Api와 요구사항 적용 (#63) (#71)
    즐겨찾기 전체 기능 바뀐 Api와 요구사항 적용

--- src/main/java/com/project/comgle/dto/response/BookMarkFolderResponseDto.java
+++ src/main/java/com/project/comgle/dto/response/BookMarkFolderResponseDto.java
@@ -1,13 +1,24 @@
 package com.project.comgle.dto.response;
 
 import com.project.comgle.entity.BookMarkFolder;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class BookMarkFolderResponseDto {
+    private Long id;
     private String bookMarkFolderName;
 
-    public BookMarkFolderResponseDto(BookMarkFolder bookMarkFolderName) {
-        this.bookMarkFolderName = bookMarkFolderName.getBookMarkFolderName();
+    @Builder
+    private BookMarkFolderResponseDto(Long id, String bookMarkFolderName) {
+        this.id = id;
+        this.bookMarkFolderName = bookMarkFolderName;
+    }
+
+    public static BookMarkFolderResponseDto of(Long id, String bookMarkFolderName){
+        return BookMarkFolderResponseDto.builder()
+                .id(id)
+                .bookMarkFolderName(bookMarkFolderName)
+                .build();
     }
 }
