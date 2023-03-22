0d48514754de782f7afb619743f4be6cbffc8830
hunny-A <nahyun7656a@gmail.com>
2023-03-22 15:10:17 +0900

    Refactor : 예외처리 (#92)
     - 구조 잡기
     - 즐겨찾기 예외처리 변경 완료

--- src/main/java/com/project/comgle/controller/BookMarkController.java
+++ src/main/java/com/project/comgle/controller/BookMarkController.java
@@ -1,8 +1,8 @@
 package com.project.comgle.controller;
 
+import com.project.comgle.dto.common.SuccessResponse;
 import com.project.comgle.dto.request.BookMarkFolderRequestDto;
 import com.project.comgle.dto.response.BookMarkFolderResponseDto;
-import com.project.comgle.dto.response.MessageResponseDto;
 import com.project.comgle.dto.response.PostResponseDto;
 import com.project.comgle.security.UserDetailsImpl;
 import com.project.comgle.service.BookMarkService;
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,21 +24,21 @@ public class BookMarkController {
     @Operation(summary = "즐겨찾기 폴더 추가 API", description = "해당 MEMBER의 즐겨찾기 폴더를 추가합니다. 폴더는 최대 100개까지 가능합니다.")
     @ResponseStatus(value = HttpStatus.OK)
     @PostMapping("/bookmark/folders")
-    public ResponseEntity<MessageResponseDto> createBookMarkFolder(@RequestBody BookMarkFolderRequestDto bookMarkFolderRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public SuccessResponse createBookMarkFolder(@RequestBody BookMarkFolderRequestDto bookMarkFolderRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return bookMarkService.createBookMarkFolder(bookMarkFolderRequestDto.getBookMarkFolderName(), userDetails.getMember());
     }
 
     @Operation(summary = "즐겨찾기 폴더 삭제 API", description = "해당 MEMBER의 즐겨찾기 폴더를 삭제합니다")
     @ResponseStatus(value = HttpStatus.OK)
     @DeleteMapping("/bookmark/folders/{folder-id}")
-    public ResponseEntity<MessageResponseDto> delBookMarkFolder(@PathVariable(name = "folder-id") Long folderId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public SuccessResponse delBookMarkFolder(@PathVariable(name = "folder-id") Long folderId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return bookMarkService.delBookMarkFolder(folderId, userDetails.getMember());
     }
 
     @Operation(summary = "즐겨찾기 폴더 수정 API", description = "해당 MEMBER가 지정한 즐겨찾기 폴더명을수정합니다")
     @ResponseStatus(value = HttpStatus.OK)
     @PutMapping("/bookmark/folders/{folder-id}")
-    public ResponseEntity<MessageResponseDto> updateBookMarkFolder(@PathVariable(name = "folder-id") Long folderId, @RequestBody BookMarkFolderRequestDto bookMarkFolderRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public SuccessResponse updateBookMarkFolder(@PathVariable(name = "folder-id") Long folderId, @RequestBody BookMarkFolderRequestDto bookMarkFolderRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return bookMarkService.updateBookMarkFolder(folderId, bookMarkFolderRequestDto.getBookMarkFolderName(), userDetails.getMember());
     }
 
@@ -53,14 +52,14 @@ public class BookMarkController {
     @Operation(summary = "즐겨찾기 추가 API", description = "해당 폴더에 즐겨찾기를 추가합니다.")
     @ResponseStatus(value = HttpStatus.OK)
     @PostMapping("/bookmark/folders/{folder-id}/bookmarks/{post-id}")
-    public ResponseEntity<MessageResponseDto> postBookMark(@PathVariable(name = "folder-id") Long folderId, @PathVariable(name = "post-id") Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public SuccessResponse postBookMark(@PathVariable(name = "folder-id") Long folderId, @PathVariable(name = "post-id") Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return bookMarkService.postBookMark(folderId, postId, userDetails.getMember());
     }
 
     @Operation(summary = "즐겨찾기 취소 API", description = "해당 폴더에 즐겨찾기를 취소합니다.")
     @ResponseStatus(value = HttpStatus.OK)
     @DeleteMapping("/bookmark/folders/{folder-id}/bookmarks/{post-id}")
-    public ResponseEntity<MessageResponseDto> delBookMark(@PathVariable(name = "folder-id") Long folderId, @PathVariable(name = "post-id") Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public SuccessResponse delBookMark(@PathVariable(name = "folder-id") Long folderId, @PathVariable(name = "post-id") Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return bookMarkService.delBookMark(folderId, postId, userDetails.getMember());
     }
 
---
+++ src/main/java/com/project/comgle/exception/ExceptionEnum.java
@@ -0,0 +1,81 @@
+package com.project.comgle.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ExceptionEnum {
+
+/* 로그인 */
+    // 잘못된 이메일 입니다.
+    WRONG_EMAIL(400,"Invalid email."),
+    // 잘못된 비밀번호 입니다.
+    WORNG_PASSWORD(400,"Invalid password."),
+
+    // 유효한 이메일 형식이 아닙니다.
+    INVALID_EMAIL_REG(400,"Invalid email format."),
+    // 비밀번호는 대소문자, 숫자, 특수문자를 포함하여 8-32자 이내여야 합니다.
+    INVALID_PASSWD_REG(400,"Password must be 8-32 characters long, including case, number, and special characters."),
+
+    //토큰이 유효하지 않습니다.
+    INVALID_TOKEN(400,"Token is not valid."),
+    // 잘못된 요청값입니다
+    INVALID_VALUE(400,"Invalid request value."),
+
+
+
+/* 계정 권한 */
+    // ADMIN 권한이 필요합니다.
+    REQUIRED_ADMIN_POSITION(400, "ADMIN permissions required."),
+    // ADMIN 계정으로 직책을 변경할 수 없습니다.
+    IMMULATABLE_TO_ADMIN(400,"You cannot change your position with the ADMIN account."),
+    // ADMIN 계정은 직책을 변경할 수 없습니다.
+    IMMULATABLE_ADMIN_POSITION(400, "The ADMIN account cannot change its position."),
+    // 수정할 수 있는 권한이 없습니다.
+    INVALID_PERMISSION_TO_MODIFY(400,"You do not have permission to modify."),
+    // 읽을 수 있는 권한이 없습니다.
+    INVALID_PERMISSION_TO_READ(400,"You do not have permission to read."),
+
+
+/* 존재하지 않는 요소 */
+    // 해당 직책이 존재하지 않습니다.
+    NOT_EXIST_POSITION(400,"The position does not exist."),
+    // 해당 계정(사용자)이 존재하지 않습니다.
+    NOT_EXIST_MEMBER(400,"That account(member) doesn't exist."),
+    // 해당 회사가 존재하지 않습니다.
+    NOT_EXIST_COMPANY(400, "The company does not exist."),
+    // 해당 폴더가 존재하지 않습니다.
+    NOT_EXIST_FOLDER(400,"The folder does not exist."),
+    // 해당 게시글이 존재하지 않습니다.
+    NOT_EXIST_POST(400,"The post does not exist."),
+    // 해당 카테고리가 존재하지 않습니다.
+    NOT_EXIST_CATEGORY(400,"The category does not exist."),
+    // 해당 댓글이 존재하지 않습니다.
+    NOT_EXIST_COMMENT(400,"The comment does not exist."),
+
+/* 중복 */
+    // 중복된 회사가 존재합니다.
+    DUPLICATE_COMPANY(400, "Duplicate company exists."),
+    // 중복된 이메일이 존재합니다.
+    DUPLICATE_EMAIL(400, "Duplicate email exists."),
+    // 중복된 사용자가 존재합니다.
+    DUPLICATE_MEMBER(400, "Duplicate member exists."),
+    // 중복된 게시글이 존재합니다.
+    DUPLICATE_POST(400, "Duplicate post exists."),
+    // 중복된 폴더가 존재합니다.
+    DUPLICATE_FOLDER(400, "Duplicate folder exists."),
+    // 중복된 카테고리가 존재합니다.
+    DUPLICATE_CATEGORY(400,"Duplicate category exists."),
+
+/* 그 외 */
+    // 최대 폴더 갯수를 초과하였습니다.
+     EXCEED_FOLDER_NUM(400,"Maximum number of folders exceeded");
+
+
+    private final int code;
+    private final String msg;
+
+    ExceptionEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
