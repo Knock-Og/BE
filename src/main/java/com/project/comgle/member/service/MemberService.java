2ab63743f4f4b0b5ac9bd5bb186dd1d4373b38c6
hunny-A <nahyun7656a@gmail.com>
2023-04-08 20:34:28 +0900

    Feat : 비밀번호 변경 기능 (#152)
    - 비밀번호 확인 기능
    - 비밀번호 변경 기능
    - LoginRequestDto 래팩토링

--- src/main/java/com/project/comgle/member/service/MemberService.java
+++ src/main/java/com/project/comgle/member/service/MemberService.java
@@ -1,15 +1,16 @@
 package com.project.comgle.member.service;
 
-import com.project.comgle.admin.dto.SignupRequestDto;
 import com.project.comgle.company.dto.CompanyRequestDto;
 import com.project.comgle.company.entity.Company;
 import com.project.comgle.company.repository.CompanyRepository;
 import com.project.comgle.global.common.response.MessageResponseDto;
+import com.project.comgle.global.common.response.SuccessResponse;
 import com.project.comgle.global.exception.CustomException;
 import com.project.comgle.global.exception.ExceptionEnum;
 import com.project.comgle.global.utils.JwtUtil;
 import com.project.comgle.member.dto.LoginRequestDto;
 import com.project.comgle.member.dto.MemberResponseDto;
+import com.project.comgle.member.dto.PasswordRequestDto;
 import com.project.comgle.member.entity.Member;
 import com.project.comgle.member.entity.PositionEnum;
 import com.project.comgle.member.repository.MemberRepository;
@@ -84,6 +85,31 @@ public class MemberService {
         }
 
         return memberResponseDtos;
+    }
+
+    @Transactional(readOnly = true)
+    public void checkPwd(String pwd, Member member){
+
+        Optional<Member> findMember = memberRepository.findById(member.getId());
+
+        if(!passwordEncoder.matches(pwd, findMember.get().getPassword())){
+            throw new CustomException(ExceptionEnum.WORNG_PASSWORD);
+        }
+    }
+
+    @Transactional
+    public SuccessResponse updatePwd(PasswordRequestDto passwordRequestDto, Member member){
+
+        Optional<Member> findMember = memberRepository.findById(member.getId());
+
+        if(passwordEncoder.matches(passwordRequestDto.getPassword(), findMember.get().getPassword())){
+            throw new CustomException(ExceptionEnum.DUPLICATE_PASSWORD);
+        }
+
+        String newPwdEndcode = passwordEncoder.encode(passwordRequestDto.getPassword());
+        findMember.get().updatePwd(newPwdEndcode);
+
+        return SuccessResponse.of(HttpStatus.OK, "Update Password Successful");
     }
 
 }
