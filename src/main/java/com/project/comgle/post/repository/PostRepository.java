e799c5a784e2417bd9c68e1c4582bda65b73503b
Yeowon KIM <kkyoo152@gmail.com>
2023-04-09 16:11:54 +0900

    Feat : 검색 결과 필터 기능 (#158)
    * Fix : build.gradle 수정 적용
    
    * Feat : 검색 결과 정렬 #154

--- src/main/java/com/project/comgle/post/repository/PostRepository.java
+++ src/main/java/com/project/comgle/post/repository/PostRepository.java
@@ -1,27 +1,16 @@
 package com.project.comgle.post.repository;
 
 import com.project.comgle.member.entity.Member;
-import com.project.comgle.member.entity.QMember;
 import com.project.comgle.post.entity.Post;
-import com.project.comgle.post.entity.QPost;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import static com.project.comgle.admin.entity.QCategory.category;
-import static com.querydsl.jpa.JPAExpressions.select;
-import static com.querydsl.jpa.JPAExpressions.selectFrom;
-import static com.querydsl.jpa.JPAExpressions.*;
-import static com.querydsl.core.types.dsl.Expressions.*;
-import com.querydsl.core.types.Predicate;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long>{
 
     Optional<Post> findByIdAndMember(Long postId, Member member);
     List<Post> findAllByMember(Member member);
