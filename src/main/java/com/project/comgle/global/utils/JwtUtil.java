ee5d047d49ba0f749813ba93ff7e98e1d7fb7518
hunny-A <nahyun7656a@gmail.com>
2023-04-15 13:05:12 +0900

    Refactor : Jwt 토큰 시간 변경 (1시간 > 24시간)

--- src/main/java/com/project/comgle/global/utils/JwtUtil.java
+++ src/main/java/com/project/comgle/global/utils/JwtUtil.java
@@ -27,7 +27,7 @@ public class JwtUtil {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String AUTHORIZATION_KEY = "auth";
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long TOKEN_TIME = 60 * 60 * 1000L;
+    private static final long TOKEN_TIME = 60 * 60 * 24 * 1000L;
 
     @Value("${jwt.secret.key}")
     private String secretKey;
