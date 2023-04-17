1abc088973a09c8f6d12f57c7785b167e849091d
hunny-A <nahyun7656a@gmail.com>
2023-04-18 01:43:23 +0900

    fix : admin url path를 통한 접근 제어

--- src/main/java/com/project/comgle/global/config/WebSecurityConfig.java
+++ src/main/java/com/project/comgle/global/config/WebSecurityConfig.java
@@ -28,6 +28,7 @@ import java.util.Arrays;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
+
     private final JwtUtil jwtUtil;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
@@ -40,30 +41,28 @@ public class WebSecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> {
             web.ignoring()
-                    //.requestMatchers(PathRequest.toH2Console())
                     .antMatchers("/h2-console/**", "/swagger-ui/**",  "/v3/api-docs/**","/api-docs/**", "/s3.amazonaws.com/**")
                     .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
         };
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //  Spring Security 필터 체인을 구성하는 인터페이스다.
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.cors().configurationSource(request -> {
             CorsConfiguration cors = new CorsConfiguration();
 
             cors.setAllowedOriginPatterns(Arrays.asList("*"));
-
             cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
             cors.setAllowedHeaders(Arrays.asList("*"));
             cors.addExposedHeader("Authorization");
             cors.setAllowCredentials(true);
+
             return cors;
         });
 
-        http.csrf().disable(); // CSRF 방어 기능을 비활성화한다.
+        http.csrf().disable();
 
-        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests() //  요청에 대한 보안 검사를 구성한다.
@@ -73,13 +72,13 @@ public class WebSecurityConfig {
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/member/**").permitAll()
                 .antMatchers("/connect/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated() // 나머지 URL에 대한 접근 권한을 설정합니다. 인증된 사용자만 접근할 수 있다.
                 // JWT 인증/인가를 사용하기 위한 설정
                 // JwtAuthFilter를 UsernamePasswordAuthenticationFilter 이전에 실행되도록 설정한다. JwtAuthFilter는 JWT 토큰을 검증하고 인증/인가를 처리한다.
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         http.exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
-//        http.formLogin().loginPage("/api/user/login-page").permitAll(); //로그인 페이지를 설정한다
 
         return http.build();
     }
